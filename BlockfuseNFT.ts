31b85e1e0c96b8189eb5441a14a107ce8c2fe004
Sprof22 <richmondelaigwu@gmail.com>
2025-02-04 22:32:40 +0100

    Remove Blockfuse NFT and Faucet contract files

--- BlockfuseNFT.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-import { IgnitionModule, buildModule } from "@nomicfoundation/ignition-core";
-
-export default buildModule("BlockfuseNFT", (m) => {
-  // Deploy the BlockfuseNFT contract
-  const blockfuseNFT = m.contract("BlockfuseNFT", []);
-
-  return { blockfuseNFT };
-});
