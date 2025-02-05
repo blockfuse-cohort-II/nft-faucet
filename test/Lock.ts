9b22b5b9039c7988e45e53be5ce6ac52a5f4f7a2
Sprof22 <richmondelaigwu@gmail.com>
2025-02-05 17:46:50 +0100

    feat: refactor Lock tests to BlockfuseNFT

--- test/Lock.ts
+++ test/Lock.ts
@@ -1,127 +1,83 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import hre from "hardhat";
+import { ethers } from "hardhat";
+import { BlockfuseNFT, BlockfuseNFT__factory } from "../typechain-types";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("BlockfuseNFT", function () {
+  let blockfuseNFT: BlockfuseNFT;
+  let owner: any;
+  let addr1: any;
+  let addr2: any;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  beforeEach(async function () {
+    [owner, addr1, addr2] = await ethers.getSigners();
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.ethers.getSigners();
-
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+    const BlockfuseNFTFactory: BlockfuseNFT__factory = <BlockfuseNFT__factory>(
+      await ethers.getContractFactory("BlockfuseNFT")
+    );
+    blockfuseNFT = await BlockfuseNFTFactory.deploy();
+    await blockfuseNFT.deploymentTransaction();
+  });
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+    it("Should set the correct name and symbol", async function () {
+      expect(await blockfuseNFT.name()).to.equal("BlockfuseNFT");
+      expect(await blockfuseNFT.symbol()).to.equal("BFN");
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
+    it("Should initialize tokenCounter to 0", async function () {
+      expect(await blockfuseNFT.tokenCounter()).to.equal(0);
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+  describe("Minting", function () {
+    it("Should allow an address to mint an NFT if they haven't already", async function () {
+      const initialTokenCounter = await blockfuseNFT.tokenCounter(); 
+      await blockfuseNFT.mint(addr1.address);
+      expect(await blockfuseNFT.tokenCounter()).to.equal(initialTokenCounter + 1n); // Use 1n for bigint addition
+      expect(await blockfuseNFT.ownerOf(0)).to.equal(addr1.address);
+      expect(await blockfuseNFT.hasMinted(addr1.address)).to.equal(true);
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+    it("Should prevent an address from minting more than one NFT", async function () {
+      await blockfuseNFT.mint(addr1.address);
+      await expect(blockfuseNFT.mint(addr1.address)).to.be.revertedWith("You already own an NFT");
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("Should allow multiple addresses to mint unique NFTs", async function () {
+      await blockfuseNFT.mint(addr1.address);
+      await blockfuseNFT.mint(addr2.address);
 
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+      expect(await blockfuseNFT.ownerOf(0)).to.equal(addr1.address);
+      expect(await blockfuseNFT.ownerOf(1)).to.equal(addr2.address);
+      expect(await blockfuseNFT.hasMinted(addr1.address)).to.equal(true);
+      expect(await blockfuseNFT.hasMinted(addr2.address)).to.equal(true);
     });
   });
+
+  describe("Token Existence", function () {
+    it("Should return true for existing tokens", async function () {
+      await blockfuseNFT.mint(addr1.address);
+      expect(await blockfuseNFT.exists(0)).to.equal(true);
+    });
+
+    it("Should return false for non-existing tokens", async function () {
+      expect(await blockfuseNFT.exists(0)).to.equal(false);
+      await blockfuseNFT.mint(addr1.address);
+      expect(await blockfuseNFT.exists(1)).to.equal(false);
+    });
+  });
+
+  // describe("Ownership", function () {
+  //   it("Should restrict minting to the owner only if desired (optional)", async function () {
+  //     /*
+  //     Uncomment the following lines if you modify the `mint` function to be `onlyOwner`
+      
+  //     await expect(blockfuseNFT.connect(addr1).mint(addr1.address)).to.be.revertedWith("Ownable: caller is not the owner");
+
+  //     // Owner can mint
+  //     await blockfuseNFT.mint(addr1.address);
+  //     expect(await blockfuseNFT.ownerOf(0)).to.equal(addr1.address);
+  //     */
+  //   });
+  // });
 });
